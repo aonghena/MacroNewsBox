a91cd77b85f0cdbf0b1949ce38ca5388ec3090e2
Alex O <8991237+aonghena@users.noreply.github.com>
2020-05-21 23:03:00 -0400

    Fixed Formatting

--- main.js
+++ main.js
@@ -39,8 +39,9 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadFile('index.html')
+  win.webContents.openDevTools();
+
 }
 
 
 app.whenReady().then(createWindow)
-
